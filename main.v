e0e8d914a33ed3843a8f4336fa107a312b28ea20
João Gabriel Santos Silva <145305652+uJotaQ@users.noreply.github.com>
2024-05-15 17:35:47 -0300

    Update main.v

--- main.v
+++ main.v
@@ -1,6 +1,6 @@
-module main(CLK, C1, C2, C3, C4, C5, L1, L2, L3, L4, L5, L6, L7);
+module main(AS, GT, CLK, C1, C2, C3, C4, C5, L1, L2, L3, L4, L5, L6, L7);
 	
-	input CLK;
+	input CLK, AS, GT;
 	output C1, C2, C3, C4, C5, L1, L2, L3, L4, L5, L6, L7;
 	
 	wire Q1, Q2, Q3, Q1_, Q2_, Q3_;
@@ -20,24 +20,57 @@ module main(CLK, C1, C2, C3, C4, C5, L1, L2, L3, L4, L5, L6, L7);
 	and(C5, Q1_, Q2_, Q3);
 	
 // LIGANDO A LETRA "A" NA MATRIZ
-//	or(L2, C2, C3, C4);
-//	or(L3, C2, C3, C4);
-//	or(L4, C2, C3, C4);
-//	or(L6, C2, C3, C4);
-//	or(L7, C2, C3, C4);
+	or(A2, C2, C3, C4);
+	or(A3, C2, C3, C4);
+	or(A4, C2, C3, C4);
+	or(A6, C2, C3, C4);
+	or(A7, C2, C3, C4);
+	
+	//and(A_S1, A1, AS);
+	and(A_S2, A2, AS);
+	and(A_S3, A3, AS);
+	and(A_S4, A4, AS);
+	//and(A_S5, A5, AS);
+	and(A_S6, A6, AS);
+	and(A_S7, A7, AS);
 
 // LIGANDO A LETRA "G" NA MATRIZ
-//or(L2, C2, C3, C4, C5);
-//or(L3, C2, C3, C4, C5);
-//or(L4, C2, C3);
-//or(L5, C2, C3, C4);
-//or(L6, C2, C3, C4);
+	or(G2, C2, C3, C4, C5);
+	or(G3, C2, C3, C4, C5);
+	or(G4, C2, C3);
+	or(G5, C2, C3, C4);
+	or(G6, C2, C3, C4);
+	
+	//and(G_S1, G1, GT);
+	and(G_S2, G2, GT);
+	and(G_S3, G3, GT);
+	and(G_S4, G4, GT);
+	and(G_S5, G5, GT);
+	and(G_S6, G6, GT);
+	//and(G_S7, G7, GT);
 
 // LIGANDO O NUMERO "0" NA MATRIZ
-	or(L2, C2, C3, C4);
-	or(L3, C2, C3, C4);
-	or(L4, C2, C3, C4);
-	or(L5, C2, C3, C4);
-	or(L6, C2, C3, C4);
-
+	or(O2, C2, C3, C4);
+	or(O3, C2, C3, C4);
+	or(O4, C2, C3, C4);
+	or(O5, C2, C3, C4);
+	or(O6, C2, C3, C4);
+	
+	not(NOT_AS, AS);
+	not(NOT_GT, GT);
+	
+	//and(O_S1, O1, NOT_AS, NOT_GT);
+	and(O_S2, O2, NOT_AS, NOT_GT);
+	and(O_S3, O3, NOT_AS, NOT_GT);
+	and(O_S4, O4, NOT_AS, NOT_GT);
+	and(O_S5, O5, NOT_AS, NOT_GT);
+	and(O_S6, O6, NOT_AS, NOT_GT);
+	//and(O_S7, O7, NOT_AS, NOT_GT);
+	
+	or(L2, A_S2, G_S2, O_S2);
+	or(L2, A_S2, G_S2, O_S2);
+	or(L2, A_S2, G_S2, O_S2);
+	or(L2, A_S2, G_S2, O_S2);
+	or(L2, A_S2, G_S2, O_S2);
+	
 endmodule
