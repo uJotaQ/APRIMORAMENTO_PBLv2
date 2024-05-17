e5b49740961bbc48aa30dd95151cb379d4e376ff
João Gabriel Santos Silva <145305652+uJotaQ@users.noreply.github.com>
2024-05-16 23:47:12 -0300

    Update main.v

--- main.v
+++ main.v
@@ -26,11 +26,9 @@ module main(AS, GT, CLK, C1, C2, C3, C4, C5, L1, L2, L3, L4, L5, L6, L7);
 	or(A6, C2, C3, C4);
 	or(A7, C2, C3, C4);
 	
-	//and(A_S1, A1, AS);
 	and(A_S2, A2, AS);
 	and(A_S3, A3, AS);
 	and(A_S4, A4, AS);
-	//and(A_S5, A5, AS);
 	and(A_S6, A6, AS);
 	and(A_S7, A7, AS);
 
@@ -41,13 +39,11 @@ module main(AS, GT, CLK, C1, C2, C3, C4, C5, L1, L2, L3, L4, L5, L6, L7);
 	or(G5, C2, C3, C4);
 	or(G6, C2, C3, C4);
 	
-	//and(G_S1, G1, GT);
 	and(G_S2, G2, GT);
 	and(G_S3, G3, GT);
 	and(G_S4, G4, GT);
 	and(G_S5, G5, GT);
 	and(G_S6, G6, GT);
-	//and(G_S7, G7, GT);
 
 // LIGANDO O NUMERO "0" NA MATRIZ
 	or(O2, C2, C3, C4);
@@ -59,18 +55,17 @@ module main(AS, GT, CLK, C1, C2, C3, C4, C5, L1, L2, L3, L4, L5, L6, L7);
 	not(NOT_AS, AS);
 	not(NOT_GT, GT);
 	
-	//and(O_S1, O1, NOT_AS, NOT_GT);
 	and(O_S2, O2, NOT_AS, NOT_GT);
 	and(O_S3, O3, NOT_AS, NOT_GT);
 	and(O_S4, O4, NOT_AS, NOT_GT);
 	and(O_S5, O5, NOT_AS, NOT_GT);
 	and(O_S6, O6, NOT_AS, NOT_GT);
-	//and(O_S7, O7, NOT_AS, NOT_GT);
 	
 	or(L2, A_S2, G_S2, O_S2);
-	or(L2, A_S2, G_S2, O_S2);
-	or(L2, A_S2, G_S2, O_S2);
-	or(L2, A_S2, G_S2, O_S2);
-	or(L2, A_S2, G_S2, O_S2);
+	or(L3, A_S3, G_S3, O_S3);
+	or(L4, A_S4, G_S4, O_S4);
+	or(L5, G_S5, O_S5);
+	or(L6, A_S6, G_S6, O_S6);
+	or(L7, A_S7);
 	
 endmodule
